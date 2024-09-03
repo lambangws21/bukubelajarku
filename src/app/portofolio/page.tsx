d6dbadfc9b651108c79cc8ef03b46f23322ea4f8
Herlambang Wicaksono <lambangws21@gmail.com>
2024-09-03 20:46:32 +0700

    memperbaiki error

--- src/app/portofolio/page.tsx
+++ src/app/portofolio/page.tsx
@@ -11,7 +11,7 @@ const Home: React.FC = () => {
       <header className="relative bg-blue-500 p-8 text-center flex flex-col items-center">
         <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-white mb-4">
           {/* Ganti src dengan URL gambar profil Anda */}
-          <img src="https://via.placeholder.com/150" alt="Profile Picture" className="w-full h-full object-cover" />
+          {/* <img src="https://via.placeholder.com/150" alt="Profile Picture" className="w-full h-full object-cover" /> */}
         </div>
         <h1 className="text-4xl font-bold">Herlambang Wicaksono</h1>
         <p className="text-md mt-2">Freelancer | Operating Room Nurse</p>
@@ -27,7 +27,7 @@ const Home: React.FC = () => {
         <section id="bio" className="bg-blue-100 text-blue-900 p-6 rounded-lg shadow-md space-y-4">
           <h2 className="text-2xl font-semibold">About Me</h2>
           <p className="text-lg">
-            Hi! I'm Herlambang Wicaksono, born on August 4th, 1994, a graduate of Respati University Yogyakarta. Previously worked at RS Carolus, currently working as a freelancer.
+            Hi! I&apos;m Herlambang Wicaksono, born on August 4th, 1994, a graduate of Respati University Yogyakarta. Previously worked at RS Carolus, currently working as a freelancer.
           </p>
           <p className="text-lg">
             I specialize in operating room nursing and room nursing. I am known for being adaptable and having a creative mind. My goal is to continuously improve my skills and explore new opportunities to excel in my field.
