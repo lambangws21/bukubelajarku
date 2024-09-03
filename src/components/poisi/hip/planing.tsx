72af0952a902d3a9f4254c660df2491bf7d429d1
Herlambang Wicaksono <lambangws21@gmail.com>
2024-09-03 21:46:22 +0700

    menambahkan items gambar

--- src/components/poisi/hip/planing.tsx
+++ src/components/poisi/hip/planing.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import PreOperasi from "@/app/procedure/posisihip/img/preop.jpg";
 import PostOperasi from "@/app/procedure/posisihip/img/postOp.jpg";
+import Gambar1 from "@/app/procedure/posisihip/img/62_Pr010_i010.png";
+import Gambar2 from "@/app/procedure/posisihip/img/62_Pr010_i020.png";
 
 export const PlaningCard = () => {
   return (
@@ -11,22 +13,38 @@ export const PlaningCard = () => {
         <CardTitle>Planning Pre Operasi</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="flex items-center justify-center flex-col">
+        <Card className="flex items-center justify-center mb-3 flex-col">
           <Image
             src={PreOperasi}
             alt="Penjelasan Gambar Pertama"
-            width={468}
-            height={826}
-            className="rounded-md w-auto h-auto"
+            width={268}
+            height={126}
+            className="rounded-md w-auto h-auto hover:scale-150 transition ease-in-out duration-200"
           />
           <Image
             src={PostOperasi}
             alt="Penjelasan Gambar Pertama"
+            width={268}
+            height={126}
+            className="rounded-md w-auto h-auto hover:scale-150 transition ease-in-out duration-200"
+          />
+        </Card>
+        <Card className="flex items-center justify-center">
+        <Image
+            src={Gambar1}
+            alt="Penjelasan Gambar Pertama"
             width={468}
             height={826}
             className="rounded-md w-auto h-auto"
           />
-        </div>
+           <Image
+            src={Gambar2}
+            alt="Penjelasan Gambar Pertama"
+            width={468}
+            height={826}
+            className="rounded-md w-auto h-auto"
+          />
+        </Card>
       </CardContent>
     </Card>
   );
