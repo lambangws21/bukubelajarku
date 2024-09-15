f89657594af5a0d8212e34abfa3acc4bf387b7ca
Herlambang Wicaksono <lambangws21@gmail.com>
2024-09-15 13:27:20 +0700

    memperbaiki error

--- src/app/googlesheetform/page.tsx
+++ src/app/googlesheetform/page.tsx
@@ -24,7 +24,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { useToast } from "@/components/ui/use-toast";
+import { useToast } from "@/hooks/use-toast";
 import { motion } from "framer-motion";
 
 // Schema untuk validasi form
