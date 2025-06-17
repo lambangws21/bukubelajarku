05a4c41c5140c9594d805ed072eeee60dbb00e57
lambangws <macbookairm1@lambangws.local>
2025-06-17 15:52:34 +0800

    merebubah @route

--- src/components/Dasboard/DasboardIntertain/DasboardPage.tsx
+++ src/components/Dasboard/DasboardIntertain/DasboardPage.tsx
@@ -10,9 +10,9 @@ import { Plus } from 'lucide-react';
 
 import FilterBar from '@/components/Dasboards/DasboardIntertain/FilterBar';
 import KPIStats from '@/components/Dasboards/DasboardIntertain/KPIStats';
-import MainCharts from './MainCharts';
-import DataTable, { DataItem } from './DataTabel';
-import FormModal from './FormModal';
+import MainCharts from '@/components/Dasboards/DasboardIntertain/MainCharts';
+import DataTable, { DataItem } from '@/components/Dasboards/DasboardIntertain/DataTabel';
+import FormModal from '@/components/Dasboards/DasboardIntertain/FormModal';
 
 const API_URL = 'https://script.google.com/macros/s/AKfycbxkbSV9Qexu6t7pyT28vqjxTTcnKb56Ryw4StH5a_HU5yDi2LkymDyou6ZQbvwxInZGjQ/exec';
 const fetcher = (url: string) => axios.get<{ status: string; data: DataItem[] }>(url).then(r => r.data.data);
