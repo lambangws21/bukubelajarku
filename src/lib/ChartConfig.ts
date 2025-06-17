e0312219a1070a87b2a8a27cab229d6a42677a5f
lambangws <macbookairm1@lambangws.local>
2025-06-18 00:39:40 +0800

    menambahkan file

--- src/lib/ChartConfig.ts
+++ src/lib/ChartConfig.ts
@@ -1,27 +1,34 @@
-// File: lib/ChartConfig.ts
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler, // ✅ tambahkan ini
-  ArcElement
-} from 'chart.js';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip,
-  Legend,
-  Filler, // ✅ daftarkan di sini
-  ArcElement
-);
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    LineElement,
+    PointElement,
+    ArcElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler,
+    BarController,
+    LineController,
+    DoughnutController,
+  } from 'chart.js';
+  
+  // ⛔️ Penting: ini HARUS dipanggil di awal sebelum chart digunakan
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    LineElement,
+    PointElement,
+    ArcElement,
+    Title,
+    Filler,
+    Tooltip,
+    Legend,
+    BarController,
+    LineController,
+    DoughnutController
+  );
+  
