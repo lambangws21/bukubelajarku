71aa6552b000665c5e3eb6ef6155d03234ac6e82
lambangws <macbookairm1@lambangws.local>
2025-09-04 15:30:53 +0800

    mengoreksi

--- src/app/xray-view/page.tsx
+++ src/app/xray-view/page.tsx
@@ -1,10 +1,10 @@
-import XrayViewer from "@/components/xray/xrayViewer";
+// import XrayViewer from "@/components/xray/xrayViewer";
 
-export default function Page() {
-  return (
-    <div className="p-4">
-      <h1 className="text-xl font-semibold mb-2">X-Ray Viewer</h1>
-      <XrayViewer />
-    </div>
-  );
-}
+// export default function Page() {
+//   return (
+//     <div className="p-4">
+//       <h1 className="text-xl font-semibold mb-2">X-Ray Viewer</h1>
+//       <XrayViewer />
+//     </div>
+//   );
+// }
