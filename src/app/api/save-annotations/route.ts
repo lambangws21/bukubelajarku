acaac7ee5a8dacf4572fe27c1faff1e4ee25ab57
lambangws <macbookairm1@lambangws.local>
2025-09-04 15:00:50 +0800

    mengubah tampilanXX-

--- src/app/api/save-annotations/route.ts
+++ src/app/api/save-annotations/route.ts
@@ -1,14 +1,14 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const { annotations, imageName } = req.body;
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method === "POST") {
+//     const { annotations, imageName } = req.body;
 
-    console.log("Anotasi disimpan:", { imageName, annotations });
+//     console.log("Anotasi disimpan:", { imageName, annotations });
 
-    // Simpan ke database / file / Google Sheet sesuai kebutuhan
-    return res.status(200).json({ message: "Berhasil disimpan" });
-  }
+//     // Simpan ke database / file / Google Sheet sesuai kebutuhan
+//     return res.status(200).json({ message: "Berhasil disimpan" });
+//   }
 
-  res.status(405).json({ message: "Method not allowed" });
-}
+//   res.status(405).json({ message: "Method not allowed" });
+// }
